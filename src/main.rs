b8239d35435d4aae9419ed3046f5d705e1eaa1f5
Sstandby <standbyy9@gmail.com>
2023-11-25 20:02:25 -0500

    feat: server shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
 mod commands;
 
 use commands::*;
+use anyhow::Context as _;
 use poise::serenity_prelude as serenity;
+use shuttle_poise::ShuttlePoise;
+use shuttle_secrets::SecretStore;
 
-struct Data {}
+struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-#[tokio::main]
-async fn main() {
-    dotenv::dotenv().expect("Failed to load .env file");
+#[shuttle_runtime::main]
+async fn poise(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> ShuttlePoise<Data, Error> {
+    // Get the discord token set in `Secrets.toml`
+    let discord_token = secret_store
+        .get("DISCORD_TOKEN")
+        .context("'DISCORD_TOKEN' was not found")?;
+
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: vec![pxl::apps()],
             ..Default::default()
         })
-        .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
+        .token(discord_token)
         .intents(serenity::GatewayIntents::non_privileged())
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 Ok(Data {})
             })
-        });
+        })
+        .build()
+        .await
+        .map_err(shuttle_runtime::CustomError::new)?;
 
-    framework.run().await.unwrap();
+    Ok(framework.into())
 }
